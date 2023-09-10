3bd51a2e43301ed2c578c4961b6f23f67805fc97
BiveKKarki <karki777bivek@gmail.com>
2023-09-11 02:13:41 +0545

    landing page complete

--- src/conponents/Searchbar.jsx
+++ src/conponents/Searchbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { FiSearch } from 'react-icons/fi'
+import { AiOutlinePlus } from 'react-icons/ai'
 const Searchbar = () => {
   return (
     <>
-        <div className='flex relative items-center'>
+    <div className='flex gap-2'>
+        <div className='flex relative items-center flex-grow'>
             <FiSearch className=' ml-1 text-white text-2xl absolute'/>
             <input 
             type='text' 
             className='flex-grow h-10 border pl-10 text-white bg-transparent border-white rounded-md' />
         </div>
+        
+            <AiOutlinePlus className="text-5xl cursor-pointer text-white"/>
+        
+      </div>
     </>
   )
 }
