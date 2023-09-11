df9224a719312963da505161033dfe30c1e79455
BiveKKarki <karki777bivek@gmail.com>
2023-09-12 00:51:43 +0545

    get data from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './conponents/Navbar'
 import Searchbar from './conponents/searchbar'
-
+import {collection, getDocs} from 'firebase/firestore'
+import { db } from './config/firebase'
 
 const App = () => {
+
+const [contacts, setContacts] = useState();
+
+useEffect(() => {
+
+  const getContacts = async () => {
+    try {
+      const contactRef= collection(db, "contacts");
+      const contactsSnapshot = await getDocs(contactRef);
+      const contactLists = contactsSnapshot.docs.map((doc) => {
+        return{
+          id:doc.id,
+          ...doc.data(),
+        };
+      });
+      setContacts(contactLists);
+      // console.log(contactLists);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  getContacts();
+
+}, []);
+
   return (
     <>
     <div className='mx-auto max-w-[370px] px-4'>
       <Navbar />
       <Searchbar/>
-      
+      <div className=''>
+        {contacts.map((contact) => (
+          <div key={contact.id}></div>
+        ))}
+      </div>
     </div>
       
     </>
