5b71b833f6a066d8442e26af4048684113b153c7
BiveKKarki <karki777bivek@gmail.com>
2023-09-16 18:41:59 +0545

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './conponents/Navbar'
-import Searchbar from './conponents/searchbar'
+// import Searchbar from './conponents/searchbar'
 import {collection, getDocs} from 'firebase/firestore'
 import { db } from './config/firebase'
+
+import { FiSearch } from 'react-icons/fi'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 import ContactCard from './conponents/ContactCard'
 import Modal from './conponents/Modal'
@@ -52,7 +55,21 @@ useEffect(() => {
    
     <div className='mx-auto max-w-[370px] px-4'>
       <Navbar />
-      <Searchbar onOpen={onOpen} />
+      {/* <Searchbar onOpen={onOpen} /> */}
+
+      <div className='flex gap-2'>
+        <div className='flex relative items-center flex-grow'>
+            <FiSearch className=' ml-1 text-white text-2xl absolute'/>
+            <input 
+            type='text' 
+            className='flex-grow h-10 border pl-10 text-white bg-transparent border-white rounded-md' />
+        </div>
+        
+            <AiOutlinePlusCircle onClick={onOpen} className="text-4xl cursor-pointer text-white"/>
+        
+      </div>
+
+
       <div className='mt-4 flex gap-2 flex-col' >
         { contacts?.map((contact) => (
               <ContactCard key={contact.id} contact={contact} />
--- src/conponents/Modal.jsx
+++ src/conponents/Modal.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+// import { AiOutlinePlusClose } from 'react-icons/ai'
 
 const Modal = ({onClose, isOpen, children}) => {
   return (
     <>
   
-        { isOpen && <div className='min-h-[200px] max-w-[80%] p-4 bg-white '>
+        { isOpen && 
+        <div className='min-h-[200px] max-w-[80%] p-4 bg-white '>
           <div className='flex '>
-            <AiOutlinePlusClose className="text-2xl justify-end"  />
+            
+            <AiOutlinePlusClose onClick={onClose} className="text-2xl justify-end"  />
           </div>
-        </div> }
+        </div> 
+        }
     </>
   )
 };
