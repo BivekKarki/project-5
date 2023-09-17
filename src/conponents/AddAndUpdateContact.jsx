3aae1650429140d1428e651500f7c5deb9ea956e
BiveKKarki <karki777bivek@gmail.com>
2023-09-18 02:23:29 +0545

    add function

--- src/conponents/AddAndUpdateContact.jsx
+++ src/conponents/AddAndUpdateContact.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import Modal from './Modal'
+import {Field, Form, Formik} from 'formik';
+import Modal from './Modal';
 
 const AddAndUpdateContact = ({isOpen, onClose}) => {
   return (
     <>
-    <Modal isOpen={isOpen} onClose={onClose} > Hello</Modal>
-   
+    <Modal isOpen={isOpen} onClose={onClose} >
+        <Formik>
+            <Form className='flex flex-col gap-3'>
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor='name'>Name</label>
+                    <Field name="name" className="border h-10"/>
+                </div>
+                
+                <div className='flex flex-col gap-1'>
+                    <label htmlFor='email'>Email</label>
+                    <Field name="email"  className="border h-10"/>
+                </div>
+                <button className='bg-orange px-3 py-1.5 border self-end'>add contact</button>
+            </Form>
+        </Formik>
+   </Modal>
     </>
   )
 }
